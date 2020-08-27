3025b5d38a22008d778a70b7bd4deafb551b4207
thomas.mcauliffe <tjamesmac@gmail.com>
2020-08-27 13:09:19 -0700

    Add styled-components

---
+++ front/src/app/style.js
@@ -0,0 +1,39 @@
+import styled, { keyframes } from "styled-components";
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${AppLogoSpin} infinite 20s linear;
+  }
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
+
+export { AppContainer, AppLogoSpin, AppLogo, AppHeader, AppLink };
